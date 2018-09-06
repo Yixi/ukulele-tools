db9954440a5756e0dbde01368125dbbfac8bb1c8
Yixi <lyh1112@gmail.com>
2018-09-06 09:26:07 +0800

    change the deploy script

--- build/gh-pages-publish.js
+++ build/gh-pages-publish.js
@@ -1,7 +1,12 @@
 const ghPages = require('gh-pages')
 const path = require('path')
 
-ghPages.publish(path.resolve(__dirname, '../dist'), err => {
+ghPages.publish(path.resolve(__dirname, '../dist'), {
+  user: {
+    name: 'Deploy',
+    email: 'deploy@deploy.com'
+  }
+}, err => {
   if (err) {
     console.error('publish fail')
     console.error(err)
